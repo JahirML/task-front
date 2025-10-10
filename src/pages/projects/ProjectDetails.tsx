1a8001f6f855b5b4e26c07e27ffaabdd06b11f75
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-09 20:09:32 -0600

    Create Modal

--- src/pages/projects/ProjectDetails.tsx
+++ src/pages/projects/ProjectDetails.tsx
@@ -1,10 +1,46 @@
 import useGetProjectById from "@/hooks/useGetProjectById";
+import Modal from "@/ui/Modal";
 import Spinner from "@/ui/Spinner";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function ProjectDetails() {
-  const { project, isLoading, isError } = useGetProjectById();
+  const { project, isLoading } = useGetProjectById();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const isOpenModal = Boolean(searchParams.get("newTask"));
+  function onClose() {
+    navigate("", { replace: true });
+  }
   if (isLoading) return <Spinner />;
-  return <div>s</div>;
+  if (project)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{project.projectName}</h1>
+        <p className="mt-5 text-2xl font-light text-gray-500">
+          {project.description}
+        </p>
+        <nav className="my-5 flex gap-3">
+          <button
+            onClick={() => setSearchParams({ newTask: "true" })}
+            className="cursor-pointer bg-purple-400 px-10 py-3 text-xl font-bold text-white transition-all hover:bg-purple-500"
+          >
+            Agregar tarea
+          </button>
+        </nav>
+        {isOpenModal && (
+          <Modal onClose={onClose}>
+            <div>
+              <h3 className="mb-4 text-3xl font-black">Nueva Tarea</h3>
+              <p className="text-xl font-bold">
+                Llena el formulario y crea {""}
+                <span className="text-fuchsia-600">una tarea</span>
+              </p>
+            </div>
+          </Modal>
+        )}
+      </>
+    );
 }
 
 export default ProjectDetails;
---
+++ src/ui/Modal.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const Modal = ({ children, onClose }: ModalProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleCloseModal = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    if (e.target === ref.current) startClosing();
+  };
+
+  const startClosing = () => {
+    setIsClosing(true);
+    setTimeout(onClose, 200);
+  };
+
+  return (
+    <div
+      onClick={handleCloseModal}
+      ref={ref}
+      className={`fixed top-0 left-0 flex h-full w-full items-center justify-center bg-black/40 backdrop-blur-sm transition-opacity ${
+        isClosing ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div
+        className={`relative w-11/12 max-w-[700px] rounded-lg bg-white px-10 py-12 shadow-2xl ${
+          isClosing ? "animate-modal-hide" : "animate-modal-show"
+        }`}
+      >
+        <button
+          onClick={startClosing}
+          className="absolute top-0 right-3 cursor-pointer text-3xl font-medium text-gray-500 transition-all hover:text-gray-800"
+        >
+          ×
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
