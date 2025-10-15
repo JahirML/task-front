2fb2cae428c77af17ab5d4ba0455356568c613ea
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-14 19:54:56 -0600

    se añadieron etiquetas para los estados

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -10,10 +10,26 @@ type GroupedTasks = {
 
 const initialStatusGroups: GroupedTasks = {
   pending: [],
-  inProgress: [],
   onHold: [],
+  inProgress: [],
   underReview: [],
   completed: [],
+};
+
+const statusTranslations: { [key: string]: string } = {
+  pending: "Pendiente",
+  onHold: "En espera",
+  inProgress: "En Progreso",
+  underReview: "En revision",
+  completed: "Completado",
+};
+
+const statusColors: { [key: string]: string } = {
+  pending: "border-t-slate-500",
+  onHold: " border-t-red-500 ",
+  inProgress: "border-t-blue-500",
+  underReview: "border-t-amber-400",
+  completed: "border-t-emerald-500",
 };
 
 function TaskList({ tasks }: TaskProps) {
@@ -30,6 +46,11 @@ function TaskList({ tasks }: TaskProps) {
       <div className="flex gap-5 overflow-x-scroll pb-32 2xl:overflow-auto">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:w-1/5 2xl:min-w-0">
+            <h3
+              className={`border border-t-8 border-slate-300 bg-white p-3 text-xl font-light capitalize ${statusColors[status]}`}
+            >
+              {statusTranslations[status]}
+            </h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="pt-3 text-center text-gray-500">
