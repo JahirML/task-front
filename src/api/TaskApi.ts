bef5df55955611680bd8dc86443ac441a33f6258
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-23 07:47:39 -0600

    add getTaskById function

--- src/api/TaskApi.ts
+++ src/api/TaskApi.ts
@@ -1,16 +1,35 @@
 import { isAxiosError } from "axios";
-import type { TaskFormData, Project } from "./../types/index";
+import type { TaskFormData, Project, Task } from "./../types/index";
 import api from "@/lib/axios";
 
 type TaskApi = {
   formData: TaskFormData;
   projectId: Project["_id"];
+  taskId: Task["_id"];
 };
 
-export async function createTask({ projectId, formData }: TaskApi) {
+export async function createTask({
+  projectId,
+  formData,
+}: Pick<TaskApi, "formData" | "projectId">) {
   try {
     const url = `/projects/${projectId}/tasks`;
     const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.message) {
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function getTaskById({
+  projectId,
+  taskId,
+}: Pick<TaskApi, "projectId" | "taskId">) {
+  try {
+    const url = `projects/${projectId}/tasks/${taskId}`;
+    const { data } = await api(url);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.message) {
