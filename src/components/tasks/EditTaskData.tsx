6a5ccb835debefb2fbacff3d766f2b62f52577af
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-27 12:50:43 -0600

    Navigate to page 404 in error

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -3,14 +3,16 @@ import type { Project } from "@/types/index";
 import EditTaskModal from "./EditTaskModal";
 import useGetTaskById from "@/hooks/tasks/useGetTaskById";
 import Spinner from "@/ui/Spinner";
+import { Navigate } from "react-router-dom";
 
 type Props = {
   projectId: Project["_id"];
 };
 
 function EditTaskData({ projectId }: Props) {
-  const { task, isLoading } = useGetTaskById();
+  const { task, isLoading, isError } = useGetTaskById();
   if (isLoading) return <Spinner />;
+  if (isError) return <Navigate to={"/404"} />;
   if (task) return <EditTaskModal task={task} projectId={projectId} />;
 }
 
