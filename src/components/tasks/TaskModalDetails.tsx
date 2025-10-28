8ee832cfe155f7693640c4337563f38c22a93529
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-28 10:48:37 -0600

    implement getTask by id hook

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,28 +1,36 @@
 import useGetTaskById from "@/hooks/tasks/useGetTaskById";
 import Modal from "@/ui/Modal";
+import Spinner from "@/ui/Spinner";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function TaskModalDetails() {
-  const { task, isError, isLoading, error } = useGetTaskById();
-  // console.log(task);
+  const navigate = useNavigate();
+  const { task, isError, isLoading, error, taskId } = useGetTaskById();
+
   if (isError) {
-    toast.error(error?.message);
+    toast.error(error?.message, { toastId: "error" });
+    navigate("", { replace: true });
   }
   return (
     <>
       <Modal>
         <div>
-          <div className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white p-5 text-left align-middle">
-            <p className="text-sm text-slate-400">Agregada el: </p>
-            <p className="text-sm text-slate-400">Última actualización: </p>
-            <h3 className="my-5 text-4xl font-black text-slate-600">
-              Titulo aquí
-            </h3>
-            <p className="mb-2 text-lg text-slate-500">Descripción:</p>
-            <div className="my-5 space-y-3">
-              <label className="font-bold">Estado Actual:</label>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <div className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white p-5 text-left align-middle">
+              <p className="text-sm text-slate-400">Agregada el: </p>
+              <p className="text-sm text-slate-400">Última actualización: </p>
+              <h3 className="my-5 text-4xl font-black text-slate-600">
+                Titulo aquí
+              </h3>
+              <p className="mb-2 text-lg text-slate-500">Descripción:</p>
+              <div className="my-5 space-y-3">
+                <label className="font-bold">Estado Actual:</label>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </Modal>
     </>
