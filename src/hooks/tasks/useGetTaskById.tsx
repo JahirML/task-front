34607f26293bd6059666165aa2d621191b18485b
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-28 10:32:10 -0600

    se añadio el mensaje de error al return

--- src/hooks/tasks/useGetTaskById.tsx
+++ src/hooks/tasks/useGetTaskById.tsx
@@ -6,18 +6,20 @@ function useGetTaskById() {
   const [searchParams] = useSearchParams();
   const params = useParams();
   const projectId = params.projectId!;
-  const taskId = searchParams.get("editTask") as string;
+  const taskId =
+    searchParams.get("editTask") || (searchParams.get("viewTask") as string);
 
   const {
     data: task,
     isLoading,
     isError,
+    error,
   } = useQuery({
     queryFn: () => getTaskById({ projectId, taskId }),
     queryKey: ["task", taskId],
     enabled: !!taskId,
   });
-  return { taskId, task, isLoading, isError };
+  return { taskId, task, isLoading, isError, error };
 }
 
 export default useGetTaskById;
