23551081f7e62b1846bcb9a44bfd5636792ff912
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-28 11:31:28 -0600

    implement task information

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,40 +1,45 @@
 import useGetTaskById from "@/hooks/tasks/useGetTaskById";
 import Modal from "@/ui/Modal";
-import Spinner from "@/ui/Spinner";
+import { formatDate } from "@/utils/utils";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function TaskModalDetails() {
   const navigate = useNavigate();
-  const { task, isError, isLoading, error, taskId } = useGetTaskById();
+  const { task, isError, error } = useGetTaskById();
 
   if (isError) {
     toast.error(error?.message, { toastId: "error" });
     navigate("", { replace: true });
   }
-  return (
-    <>
-      <Modal>
-        <div>
-          {isLoading ? (
-            <Spinner />
-          ) : (
+  if (task)
+    return (
+      <>
+        <Modal>
+          <div>
             <div className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white p-5 text-left align-middle">
-              <p className="text-sm text-slate-400">Agregada el: </p>
-              <p className="text-sm text-slate-400">Última actualización: </p>
+              <p className="text-sm text-slate-400">
+                Agregada el: {formatDate(task.createdAt)}
+              </p>
+              <p className="text-sm text-slate-400">
+                Última actualización: {formatDate(task.updatedAt)}{" "}
+              </p>
               <h3 className="my-5 text-4xl font-black text-slate-600">
-                Titulo aquí
+                {task.name}
               </h3>
-              <p className="mb-2 text-lg text-slate-500">Descripción:</p>
+              <p className="mb-2 text-lg text-slate-500">
+                Descripción: {task.description}
+              </p>
               <div className="my-5 space-y-3">
-                <label className="font-bold">Estado Actual:</label>
+                <label className="font-bold">
+                  Estado Actual: {task.status}
+                </label>
               </div>
             </div>
-          )}
-        </div>
-      </Modal>
-    </>
-  );
+          </div>
+        </Modal>
+      </>
+    );
 }
 
 export default TaskModalDetails;
