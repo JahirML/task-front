fae0fa9b0b0197946dcc50bc0be8ebd454abbb0d
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-01 20:55:26 -0600

    Se devolvio status colors al archivo para el boder top

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,6 +1,6 @@
 import type { Task } from "@/types/index";
 import TaskCard from "./TaskCard";
-import { statusColors, statusTranslations } from "@/locales/es";
+import { statusTranslations } from "@/locales/es";
 
 type TaskProps = {
   tasks: Task[];
@@ -16,6 +16,13 @@ const initialStatusGroups: GroupedTasks = {
   underReview: [],
   completed: [],
 };
+const statusColors: { [key: string]: string } = {
+  pending: "border-t-slate-500",
+  onHold: " border-t-red-500 ",
+  inProgress: "border-t-blue-500",
+  underReview: "border-t-amber-400",
+  completed: "border-t-emerald-500",
+};
 
 function TaskList({ tasks }: TaskProps) {
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -29,24 +36,27 @@ function TaskList({ tasks }: TaskProps) {
       <h2 className="my-10 text-5xl font-black">Tareas</h2>
 
       <div className="flex gap-5 overflow-x-scroll pb-32 2xl:overflow-auto">
-        {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className="min-w-[300px] 2xl:w-1/5 2xl:min-w-0">
-            <h3
-              className={`border border-t-8 border-slate-300 bg-white p-3 text-xl font-light capitalize border-t-${statusColors[status]}`}
-            >
-              {statusTranslations[status]}
-            </h3>
-            <ul className="mt-5 space-y-5">
-              {tasks.length === 0 ? (
-                <li className="pt-3 text-center text-gray-500">
-                  No Hay tareas
-                </li>
-              ) : (
-                tasks.map((task) => <TaskCard key={task._id} task={task} />)
-              )}
-            </ul>
-          </div>
-        ))}
+        {Object.entries(groupedTasks).map(([status, tasks]) => {
+          console.log(statusColors[status]);
+          return (
+            <div key={status} className="min-w-[300px] 2xl:w-1/5 2xl:min-w-0">
+              <h3
+                className={`border border-t-8 border-slate-300 bg-white p-3 text-xl font-light capitalize ${statusColors[status]}`}
+              >
+                {statusTranslations[status]}
+              </h3>
+              <ul className="mt-5 space-y-5">
+                {tasks.length === 0 ? (
+                  <li className="pt-3 text-center text-gray-500">
+                    No Hay tareas
+                  </li>
+                ) : (
+                  tasks.map((task) => <TaskCard key={task._id} task={task} />)
+                )}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- src/locales/es.ts
+++ src/locales/es.ts
@@ -5,11 +5,3 @@ export const statusTranslations: { [key: string]: string } = {
   underReview: "En revision",
   completed: "Completado",
 };
-
-export const statusColors: { [key: string]: string } = {
-  pending: "slate-500",
-  onHold: "red-500 ",
-  inProgress: "blue-500",
-  underReview: "amber-400",
-  completed: "emerald-500",
-};
