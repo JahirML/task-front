5b64f18045bf5001fc748a559e0a0a37305509e7
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-05 15:33:30 -0600

    ADD token into query key to refetch when logout

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -2,13 +2,14 @@ import { getUser } from "@/api/AuthApi";
 import { useQuery } from "@tanstack/react-query";
 
 function useAuth() {
+  const token = localStorage.getItem("AUTH_TOKEN_TASKFLOW");
   const {
     data: user,
     isLoading,
     isError,
     error,
   } = useQuery({
-    queryKey: ["user"],
+    queryKey: ["user", token],
     queryFn: getUser,
     retry: 1,
     refetchOnWindowFocus: false,
