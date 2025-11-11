013a0862532403f7ab90577e6b576fe930f1fa9b
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-11 09:40:08 -0600

    create team api file and find user function

---
+++ src/api/TeamApi.ts
@@ -0,0 +1,21 @@
+import type { Project, TeamMemberForm } from "./../types/index";
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+
+export const findUserByEmail = async ({
+  projectId,
+  formData,
+}: {
+  projectId: Project["_id"];
+  formData: TeamMemberForm;
+}) => {
+  try {
+    const url = `/projects/${projectId}/team/find`;
+    const { data } = await api.post(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
