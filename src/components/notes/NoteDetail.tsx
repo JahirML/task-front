9c77765d2a7a584f41fd299d263ecf7c6eb0af12
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-17 21:19:15 -0600

    only author an can delete notes

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,11 +1,21 @@
+import useAuth from "@/hooks/auth/useAuth";
 import type { Note } from "@/types/index";
+import Spinner from "@/ui/Spinner";
 import { formatDate } from "@/utils/utils";
+import { useMemo } from "react";
 
 type Props = {
   note: Note;
 };
 
 function NoteDetail({ note }: Props) {
+  const { user, isLoading } = useAuth();
+  const canDelete = useMemo(
+    () => user?._id === note.createdBy._id,
+    [user?._id, note.createdBy._id],
+  );
+
+  if (isLoading) return <Spinner />;
   return (
     <div className="flex items-center justify-between py-3">
       <div>
@@ -15,6 +25,14 @@ function NoteDetail({ note }: Props) {
         </p>
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+      {canDelete && (
+        <button
+          type="button"
+          className="ho cursor-pointer bg-red-600 p-2 text-xs font-bold text-white transition-colors hover:bg-red-700"
+        >
+          Eliminar
+        </button>
+      )}
     </div>
   );
 }
