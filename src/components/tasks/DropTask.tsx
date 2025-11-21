604629dfa6d34ca12c4f66616a05e74fe1c12027
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-20 20:02:28 -0600

    apply styles when task is ready to drop

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -7,8 +7,13 @@ function DropTask({ status }: Props) {
   const { isOver, setNodeRef } = useDroppable({
     id: status,
   });
+  const style = {
+    opacity: isOver ? 0.4 : undefined,
+    transition: "opacity 0.3s",
+  };
   return (
     <div
+      style={style}
       ref={setNodeRef}
       className="mt-5 grid place-content-center border-2 border-dashed border-slate-500 p-2 text-xs font-semibold text-slate-500 uppercase"
     >
